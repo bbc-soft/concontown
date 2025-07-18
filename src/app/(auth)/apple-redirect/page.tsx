914c77168b3bc4b8be16a350b94f9a9b73c72434
bigstar8181 <bigstar8181@gmail.com>
2025-07-18 16:12:40 +0900

    apple login

--- src/app/(auth)/apple-redirect/page.tsx
+++ src/app/(auth)/apple-redirect/page.tsx
@@ -31,35 +31,44 @@
 
 'use client';
 
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import { useAuthStore } from '@/stores/useAuthStore';
 import { useTranslation } from 'react-i18next';
 import BackButton from '../../../../components/common/BackButton';
+import AlertModal from '../../../../components/common/AlertModal';
 
-export default function SNSRedirectPage() {
+export default function AppleRedirectPage() {
   const { t } = useTranslation();
   const router = useRouter();
   const login = useAuthStore((state) => state.login);
 
+  const [alert, setAlert] = useState({
+    open: false,
+    title: '',
+    description: '',
+    buttonText: 'OK',
+  });
+
   useEffect(() => {
-    const stored = localStorage.getItem('auth-storage');
-    if (stored) {
-      try {
-        const parsed = JSON.parse(stored).state;
-        if (parsed?.token && parsed?.member) {
-          login(parsed.token, parsed.member, parsed.autoLogin);
-          //router.replace('/');
-          alert('handleLogin');
-          handleLogin();
-        }
-      } catch (e) {
-        console.error('❌ Failed to parse auth-storage:', e);
-        router.replace('/login');
-      }
-    } else {
-      router.replace('/login');
-    }
+    // const stored = localStorage.getItem('auth-storage');
+    // if (stored) {
+    //   try {
+    //     const parsed = JSON.parse(stored).state;
+    //     if (parsed?.token && parsed?.member) {
+    //       login(parsed.token, parsed.member, parsed.autoLogin);
+    //       //router.replace('/');
+    //       alert('handleLogin');
+    //       handleLogin();
+    //     }
+    //   } catch (e) {
+    //     console.error('❌ Failed to parse auth-storage:', e);
+    //     router.replace('/login');
+    //   }
+    // } else {
+    //   router.replace('/login');
+    // }
+    setAlert({ open: true, title: 'Caution', description: 'apple redirect', buttonText: 'OK' });
   }, []);
 
 
@@ -79,8 +88,6 @@ export default function SNSRedirectPage() {
             alert(t('loginPw.forgotPassword', 'Incorrect email or password.'));
             return;
           }
-
-          alert('login success!!');
 
           const data = await res.json();
           const user = data.user;
@@ -114,6 +121,8 @@ export default function SNSRedirectPage() {
       <div className="max-w-[430px] mx-auto min-h-screen bg-white pt-5 pb-28 px-5 text-black relative">
         <BackButton label={t('loginEmail.signUp.subSns', 'SNS Register')} />
               <p className="text-[16px] text-gray-600 my-4">{t('loginEmail.signUp.snsTitle', 'Please enter the required information to complete SNS registration.')}</p>
+
+      <AlertModal isOpen={alert.open} onClose={() => setAlert((prev) => ({ ...prev, open: false }))} title={alert.title} description={alert.description} buttonText={alert.buttonText} />
 
     </div>
   );
