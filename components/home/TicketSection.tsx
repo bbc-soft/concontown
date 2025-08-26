01a6abb75d7abb30b41a1d4700002487cc75756c
bigstar8181 <bigstar8181@gmail.com>
2025-08-26 18:44:27 +0900

    hide main

--- components/home/TicketSection.tsx
+++ components/home/TicketSection.tsx
@@ -8,9 +8,11 @@ export default function TicketSection() {
   return (
     <div>
       <TicketTabFilter selected={selected} setSelected={setSelected} />
-      {/*<div className="px-4 space-y-6">
+      {/*
+      <div className="px-4 space-y-6">
         <TicketListCard selected={selected} />
-      </div>*/}
+      </div>
+      */}
     </div>
   );
 }
