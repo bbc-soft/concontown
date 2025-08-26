ccaa32c6a0d6e5b882ca1fb311701070dbbff19f
bigstar8181 <bigstar8181@gmail.com>
2025-08-26 21:54:07 +0900

    fix: home

--- components/home/TicketSection.tsx
+++ components/home/TicketSection.tsx
@@ -20,7 +20,7 @@ export default function TicketSection() {
   return (
     <div>
       <TicketTabFilter selected={selected} setSelected={setSelected} />
-      {isAndroidApp && <div className="px-4 space-y-6">
+      {!isAndroidApp && !isIosApp && <div className="px-4 space-y-6">
         <TicketListCard selected={selected} />
       </div>}
     </div>
